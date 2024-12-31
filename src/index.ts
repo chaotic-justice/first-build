070bd3dfe90fac444adf5177cc03b0c8cb9ee5a5
chaotic-justice <deepocean.2900@gmail.com>
2024-12-31 12:06:06 -0500

    cors v2

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,31 @@
 import { Hono } from "hono"
-import { basicAuth } from "hono/basic-auth"
-import { bearerAuth } from "hono/bearer-auth"
 import { cors } from "hono/cors"
-import { jwt } from "hono/jwt"
-import type { JwtVariables } from "hono/jwt"
-
-type Variables = JwtVariables
 
 const app = new Hono()
 
-app.use("/auth/*", cors())
+// Public API (v1) - accessible to all origins
+app.use("/api/v1/*", cors())
 
-app.use("/api/*", cors())
+// Protected API (v2) - accessible only to https://mushroom.vercel.app
 app.use(
-  "/api2/*",
+  "/api/v2/*",
   cors({
-    origin: "https://kagi.vercel.app",
+    origin: "https://mushroom.vercel.app",
     allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowHeaders: ["Content-Type", "Authorization"],
     maxAge: 600,
     credentials: true,
   })
 )
-app.get("/", (c) => {
-  return c.text("Hello Hono!")
-})
 
-app.all("/api/abc", (c) => {
-  return c.json({ success: true })
-})
-app.all("/api2/abc", (c) => {
-  return c.json({ success: true })
-})
+// Public API routes
+app.get("/api/v1/hello", (c) => c.text("Hello from public API!"))
 
-// app.use(
-//   "/auth/*",
-//   basicAuth({
-//     username: "hono",
-//     password: "acoolproject",
-//   })
-// )
-
-// app.use("/auth/*", (c, next) => {
-//   const jwtMiddleware = jwt({
-//     secret: c.env.JWT_SECRET,
-//   })
-//   return jwtMiddleware(c, next)
-// })
-
-const token = "honoiscool"
-
-app.use(
-  "/auth/*",
-  bearerAuth({
-    verifyToken: async (token, c) => {
-      return token === "honoiscool"
-    },
+// Protected API routes
+app.get("/api/v2/protected", (c) =>
+  c.json({
+    msg: "Hello from protected API!",
   })
 )
-// app.use("/auth/*", bearerAuth({ token }))
-
-app.get("/auth/page", (c) => {
-  return c.text("You are authorized")
-})
 
 export default app
